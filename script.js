8bf0a42bbaa776e1b17821052ad0a9d09cc983ef
acmis1 <75743409+acmis1@users.noreply.github.com>
2023-05-19 12:55:26 +0700

    Create a custom cursor element and update its position

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const cursorImg = document.createElement('div');
+  
+    cursorImg.classList.add('cursor-img');
+  
+    document.body.appendChild(cursorImg);
+  
+    document.addEventListener('mousemove', (e) => {
+      const x = e.clientX;
+      const y = e.clientY;
+  
+      cursorImg.style.left = `${x}px`;
+      cursorImg.style.top = `${y}px`;
+    });
+  });
